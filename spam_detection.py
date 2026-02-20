020a8dd27ad09906d4032798c912a99f15a0d3a0
unknown <smetchappy@gmail.com>
2026-02-20 18:10:18 +0200

    added model prediction

--- spam_detection.py
+++ spam_detection.py
@@ -7,3 +7,18 @@ from sklearn.naive_bayes import BernoulliNB
 # Load the dataset
 data = pd.read_csv("Youtube01-Psy.csv")
 print(data.sample(5))
+
+x = np.array(data["CONTENT"])
+y = np.array(data["CLASS"])
+
+cv = CountVectorizer()
+x = cv.fit_transform(x)
+xtrain, xtest, ytrain, ytest = train_test_split(x, y, test_size=0.2, random_state=42)
+
+model = BernoulliNB()
+model.fit(xtrain, ytrain)
+print(model.score(xtest, ytest))
+
+sample = "Check this out: https://thecleverprogrammer.com/" 
+data = cv.transform([sample]).toarray()
+print(model.predict(data))
